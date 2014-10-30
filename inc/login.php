83fb9837eac30233adedba27dca95ce51bde5adb
stianiquniez <stianiquniez@gmail.com>
2014-10-30 20:49:24 +0100

    Create login.php

---
+++ inc/login.php
@@ -0,0 +1,13 @@
+<h2>Logg inn</h2>
+<form id="loginForm" action="#" method="post">
+<label>E-post</label><br />
+<input id="email2" name="email" class="inputs" type="text" /><br />
+<label>Passord</label><br />
+<input id="password2" name="password" class="inputs" type="password"/>
+</form>
+
+
+<a href="#" class="login">Logg inn</a>
+<br />
+<div class="messages2"></div>
+<div class="messagesError2"></div>
